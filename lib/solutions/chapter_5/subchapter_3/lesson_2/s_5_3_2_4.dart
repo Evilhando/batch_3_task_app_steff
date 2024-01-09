aee25f703bb6bf2e31796e6936644347cd28aa7f
Evilhando <bastthemust@gmail.com>
2024-01-09 13:54:51 +0100

    S5511

--- lib/solutions/chapter_5/subchapter_3/lesson_2/s_5_3_2_4.dart
+++ lib/solutions/chapter_5/subchapter_3/lesson_2/s_5_3_2_4.dart
@@ -9,17 +9,17 @@ class S5324 extends StatelessWidget {
   }
 }
 
-class ImageCachedThumbnail extends StatelessWidget {
-  const ImageCachedThumbnail({Key? key}) : super(key: key);
+// class ImageCachedThumbnail extends StatelessWidget {
+//   const ImageCachedThumbnail({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: CachedNetworkImage(
-        placeholder: (context, url) => (Image),
-        imageUrl:
-            'https://i.pinimg.com/736x/68/64/31/686431b9a43463be2b8f6aef19db5365.jpg',
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: CachedNetworkImage(
+//         placeholder: (context, url) => (Image),
+//         imageUrl:
+//             'https://i.pinimg.com/736x/68/64/31/686431b9a43463be2b8f6aef19db5365.jpg',
+//       ),
+//     );
+//   }
+// }
